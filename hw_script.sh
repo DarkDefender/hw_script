365025c69d0410003da68b19b5b4e2b5df32ab6d
Sebastian Parborg <darkdefende@gmail.com>
2022-10-06 18:02:00 +0200

    List all missing required programs at once

--- hw_script.sh
+++ hw_script.sh
@@ -6,13 +6,17 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-#sudo apt install dmidecode hdparm nvme-cli
-
 #Check if the required commands exist
 
-command -v dmidecode >/dev/null 2>&1 || { echo >&2 "I require dmidecode but it's not installed.  Aborting."; exit 1; }
-command -v hdparm >/dev/null 2>&1 || { echo >&2 "I require hdparm but it's not installed.  Aborting."; exit 1; }
-command -v nvme >/dev/null 2>&1 || { echo >&2 "I require nvme-cli but it's not installed.  Aborting."; exit 1; }
+missing_command=false
+
+command -v dmidecode >/dev/null 2>&1 || { echo >&2 "I require 'dmidecode' but it's not installed.  Aborting."; missing_command=true; }
+command -v hdparm >/dev/null 2>&1 || { echo >&2 "I require 'hdparm' but it's not installed.  Aborting."; missing_command=true; }
+command -v nvme >/dev/null 2>&1 || { echo >&2 "I require 'nvme-cli' but it's not installed.  Aborting."; missing_command=true; }
+
+if [ "$missing_command" = true ] ; then
+  exit 1
+fi
 
 # read-edid is supplied in this script directory
 cd `dirname "$BASH_SOURCE"`
